2524a0130b7aa1fe29836466430973a29438bd9b
Mdaher <mdaher612@gmail.com>
2019-07-27 13:12:03 -0500

    pushing to my branch

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Drops the programming_db if it already exists --
+DROP DATABASE IF EXISTS donationdb;
+
+-- Created the DB "wizard_schools_db" (only works on local connections)
+CREATE DATABASE donationdb;
+
+-- Use the DB wizard_schools_db for all the rest of the script
+USE donationdb;
