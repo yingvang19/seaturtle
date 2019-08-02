7a47fb991b926ce595d9d9a7c282874c58fa41fc
Mdaher <mdaher612@gmail.com>
2019-08-01 21:01:31 -0500

    added db and updated the config file

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Drops the donationdb if it already exists --
+DROP DATABASE IF EXISTS donationdb;
+
+-- Created the DB "donationdb" (only works on local connections)
+CREATE DATABASE donationdb;
+
+-- Use the DB donationdb for all the rest of the script
+USE donationdb;
