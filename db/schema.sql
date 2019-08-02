fd490e72fff93abb20d9d2423471b4306cd18ada
Mdaher <mdaher612@gmail.com>
2019-08-01 21:16:20 -0500

    updated config

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 -- Drops the donationdb if it already exists --
-DROP DATABASE IF EXISTS donationdb;
+DROP DATABASE IF EXISTS donation_db;
 
 -- Created the DB "donationdb" (only works on local connections)
-CREATE DATABASE donationdb;
+CREATE DATABASE donation_db;
 
 -- Use the DB donationdb for all the rest of the script
-USE donationdb;
+USE donation_db;
